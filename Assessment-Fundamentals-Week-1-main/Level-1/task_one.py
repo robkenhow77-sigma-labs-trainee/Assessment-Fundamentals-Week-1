28ab17b54ff4bea986809334c4ddb158b8d758aa
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-11-01 14:48:20 +0000

    completed taks 1

--- Assessment-Fundamentals-Week-1-main/Level-1/task_one.py
+++ Assessment-Fundamentals-Week-1-main/Level-1/task_one.py
@@ -8,12 +8,39 @@ basket = []
 
 
 def add_to_basket(item: dict) -> list:
+    """ Takes a shopping item as a dictionary and adds it to the basket list"""
     basket.append(item)
     return basket
 
 
 def generate_receipt(basket: list) -> str:
-    return  # return the receipt string
+    """ Generates a receipt based on a basket of goods. The basket is a list
+    of dictionaries which contain the product name and price.
+    """
+    if len(basket) == 0:
+        return "Basket is empty"
+    receipt = ""
+    total = 0
+    for good in basket:
+        total += good["price"]
+        if good["price"] == 0:
+            receipt += f'{good["name"]} - Free\n'
+        else:
+            receipt += f'{good["name"]} - £{format_float(good["price"])}\n'
+    return  receipt + f"Total: £{format_float(total)}"
+
+
+def format_float(number:float) -> str:
+    """ Converts a float value to a string to accurately resemble currency """
+    number = round(number, 2)
+    number_and_remainder = str(number).split('.')
+    if len(number_and_remainder) == 1:
+        return f"{number_and_remainder[0]}.00"
+    if len(number_and_remainder[1]) == 0:
+        return f"{number_and_remainder[0]}.00"
+    if len(number_and_remainder[1]) == 1:
+        return f"{number_and_remainder[0]}.{number_and_remainder[1]}0"
+    return  f"{number_and_remainder[0]}.{number_and_remainder[1]}"
 
 
 if __name__ == "__main__":
