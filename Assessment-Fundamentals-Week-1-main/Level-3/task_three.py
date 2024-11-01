62b540b05ec3d280961caf7e0527353fd9bd9cfb
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-11-01 17:09:55 +0000

    improved pylint rating

--- Assessment-Fundamentals-Week-1-main/Level-3/task_three.py
+++ Assessment-Fundamentals-Week-1-main/Level-3/task_three.py
@@ -4,44 +4,49 @@ Note: Do not add ANY variables to the global scope. This WILL break the tests.
 
 
 def generate_invoice(receipt_string: str) -> str:
-    final_receipt = "VAT RECEIPT\n\n"
+    """Generates the final vat invoice"""
+    final_receipt = "vat RECEIPT\n\n"
     original_prices = get_prices(receipt_string)
-    VAT_prices = get_VAT_prices(original_prices)
-    total_VAT = format_float(get_total_VAT(original_prices,VAT_prices))
-    VAT_receipt = create_VAT_receipt(receipt_string, VAT_prices)
-    final_receipt += f"{VAT_receipt}\nTotal: £{format_float(sum(VAT_prices))}\n"
-    final_receipt += f"VAT: £{total_VAT}\n"
-    total_price_including_VAT = get_total_price_including_VAT(receipt_string)
-    final_receipt += f"Total inc VAT: £{format_float(total_price_including_VAT)}"
+    vat_prices = get_vat_prices(original_prices)
+    total_vat = format_float(get_total_vat(original_prices,vat_prices))
+    vat_receipt = create_vat_receipt(receipt_string, vat_prices)
+    final_receipt += f"{vat_receipt}\nTotal: £{format_float(sum(vat_prices))}\n"
+    final_receipt += f"vat: £{total_vat}\n"
+    total_price_including_vat = get_total_price_including_vat(receipt_string)
+    final_receipt += f"Total inc vat: £{format_float(total_price_including_vat)}"
     if sum(original_prices) == 0:
-        return """VAT RECEIPT\n\nTotal: £0.00\nVAT: £0.00\nTotal inc VAT: £0.00"""
+        return """vat RECEIPT\n\nTotal: £0.00\nvat: £0.00\nTotal inc vat: £0.00"""
     return  final_receipt
 
 
 def get_prices(receipt_string: str) -> list:
+    """Extracts the prices from the receipt string"""
     receipt_list = receipt_string.split("\n")
     new_prices = []
     for row in receipt_list[:-1]:
-       new_prices.append(round(float(row.split("£")[1]),2))
+        new_prices.append(round(float(row.split("£")[1]),2))
     return new_prices
 
 
-def get_VAT_prices(prices:list[float]) -> list:
+def get_vat_prices(prices:list[float]) -> list:
+    """Returns a list of the prices after vat has been applied"""
     return [price * 0.8 for price in prices]
- 
-
-def get_total_VAT(original_prices:list[float], VAT_prices:list[float]) -> float:
-    return sum(original_prices) - sum(VAT_prices)
 
 
-def create_VAT_receipt(receipt_string: str, VAT_prices:list[float]) -> str:
+def get_total_vat(original_prices:list[float], vat_prices:list[float]) -> float:
+    """ Returns the total vat"""
+    return sum(original_prices) - sum(vat_prices)
+
+
+def create_vat_receipt(receipt_string: str, vat_prices:list[float]) -> str:
+    """Creates the items part of the final receipt"""
     receipt_list = receipt_string.split("\n")
     items = [row.split("£")[0] for row in receipt_list]
-    VAT_receipt = ""
-    for i in range(len(VAT_prices)):
-        VAT_receipt += f"{items[i]}£{format_float(VAT_prices[i])}\n"
-    return VAT_receipt
-    
+    vat_receipt = ""
+    for i in range(len(vat_prices)):
+        vat_receipt += f"{items[i]}£{format_float(vat_prices[i])}\n"
+    return vat_receipt
+
 
 def format_float(number:float) -> str:
     """ Converts a float value to a string to accurately resemble currency """
@@ -56,10 +61,11 @@ def format_float(number:float) -> str:
     return  f"{number_and_remainder[0]}.{number_and_remainder[1]}"
 
 
-def get_total_price_including_VAT(receipt_string: str) -> float:
+def get_total_price_including_vat(receipt_string: str) -> float:
+    """Returns the total price"""
     receipt_list = receipt_string.split("\n")
     return float(receipt_list[-1].split("£")[1])
-    
+
 
 
 if __name__ == "__main__":
