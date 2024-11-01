dcd919a4d21006c09003f2cc10d4f75f51e1ac1c
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-11-01 15:55:18 +0000

    completed task 2

--- Assessment-Fundamentals-Week-1-main/Level-2/task_two.py
+++ Assessment-Fundamentals-Week-1-main/Level-2/task_two.py
@@ -5,26 +5,51 @@ The only variable you are allowed to use in the global scope is the basket below
 
 basket = []
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
-
 
 def add_to_basket(item: dict) -> list:
-    pass
-
+    """ Takes a shopping item as a dictionary and adds it to the basket list"""
+    basket.append(item)
+    return basket
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    """ Generates a receipt based on a basket of goods. The basket is a list
+    of dictionaries which contain the product name and price.
+    """
+    if len(basket) == 0:
+        return "Basket is empty"
+    receipt = ""
+    total = get_total(basket)
+    print(basket)
+    for good in basket:
+        print(good)
+        quantity = basket.count(good)
+        receipt += f'{good["name"]} x {quantity} - £{format_float(good["price"] * quantity)}\n'
+        while basket.count(good) > 1:
+            basket.remove(good)
+    return receipt + f"Total: £{format_float(total)}"
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+
+def get_total(basket: list) -> float:
+    """ Returns the total cost"""
+    total = 0
+    for good in basket:
+         total += good["price"]
+    return total
+
+
+def format_float(number:float) -> str:
+    """ Converts a float value to a string to accurately resemble currency """
+    number = round(number, 2)
+    number_and_remainder = str(number).split('.')
+    if len(number_and_remainder) == 1:
+        return f"{number_and_remainder[0]}.00"
+    if len(number_and_remainder[1]) == 0:
+        return f"{number_and_remainder[0]}.00"
+    if len(number_and_remainder[1]) == 1:
+        return f"{number_and_remainder[0]}.{number_and_remainder[1]}0"
+    return  f"{number_and_remainder[0]}.{number_and_remainder[1]}"
+
 
 
 if __name__ == "__main__":
@@ -44,4 +69,5 @@ if __name__ == "__main__":
         "name": "Butter",
         "price": 1.20
     })
+
     print(generate_receipt(basket))
